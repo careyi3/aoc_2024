ed4786d6776c6d26509abc3dfe04114353ffe9fe
careyi3 <ian@iancarey.ie>
2024-12-01 10:57:19 +0000

    Day 1

--- solutions/1/first.rb
+++ solutions/1/first.rb
@@ -3,9 +3,24 @@
 module Day1
   module Part1
     def self.run(path, _)
+      left = []
+      right = []
+      len = 0
       FileReader.for_each_line(path) do |line|
-        puts line
+        l, r = line.split('   ').map(&:to_i)
+        left << l
+        right << r
+        len += 1
       end
+      left = left.sort
+      right = right.sort
+      diffs = []
+      len.times do |idx|
+        diff = left[idx] - right[idx]
+        diff = (-1 * diff) if diff.negative?
+        diffs << diff
+      end
+      diffs.sum
     end
   end
 end
--- solutions/1/second.rb
+++ solutions/1/second.rb
@@ -3,9 +3,28 @@
 module Day1
   module Part2
     def self.run(path, _)
+      left = {}
+      right = {}
       FileReader.for_each_line(path) do |line|
-        puts line
+        l, r = line.split('   ').map(&:to_i)
+        left[l] =
+          if left[l].nil?
+            1
+          else
+            left[l] + 1
+          end
+        right[r] =
+          if right[r].nil?
+            1
+          else
+            right[r] + 1
+          end
       end
+      sum = 0
+      left.each do |l, count|
+        sum += count * l * (right[l] || 0)
+      end
+      sum
     end
   end
 end
