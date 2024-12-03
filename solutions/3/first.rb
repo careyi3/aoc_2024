fce4fa5ce157f85c58346cc330b5e284f62bff9b
careyi3 <ian@iancarey.ie>
2024-12-03 09:01:20 +0000

    Day 3

--- solutions/3/first.rb
+++ solutions/3/first.rb
@@ -4,10 +4,10 @@ module Day3
   module Part1
     def self.run(path, _)
       memory = FileReader.read_file(path)
-      ops = memory.scan(Regexp.new('mul\(\d{1,3},\d{1,3}\)')).flatten
+      ops = memory.scan(/mul\(\d{1,3},\d{1,3}\)/).flatten
       sum = 0
       ops.each do |op|
-        l, r = op.scan(Regexp.new('\d{1,3}')).flatten.map(&:to_i)
+        l, r = op.scan(/\d{1,3}/).flatten.map(&:to_i)
         sum += (l * r)
       end
       sum
